a0fb994a6ea2c62741022e18c970355fd0cebe59
taka <koizawa.better@gmail.com>
2020-11-11 01:49:07 +0900

    add file main

---
+++ main.js
@@ -0,0 +1,10 @@
+'use strict';   //厳密なエラーチェックをする。エラーを発見しやすい
+
+ {
+  //  const score = 80;
+  //  const score = 90;
+  //  const score = 40;
+
+   const scores = [80,90,40]
+   console.log(scores);
+ }
