592a4550975221c7e41fa5fb41d94a4375f7d6f6
taka <koizawa.better@gmail.com>
2020-11-11 02:19:05 +0900

    第二回

---
+++ main.js
@@ -0,0 +1,14 @@
+'use strict';   //厳密なエラーチェックをする。エラーを発見しやすい
+
+ {
+  //  const score = 80;
+  //  const score = 90;
+  //  const score = 40;
+
+   const scores = [80,90,40]
+  //  console.log(scores[1]);
+   scores[2] = 44;
+   console.log(scores);
+  
+   console.log(scores.length);
+ }
