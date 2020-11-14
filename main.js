7a6e42db63176d5dff513047a518d1dc9e7a80c0
taka <koizawa.better@gmail.com>
2020-11-14 10:59:32 +0900

    join(),split()

--- main.js
+++ main.js
@@ -1,20 +1,9 @@
 "use strict";   //的確なエラーチェックが行われる
 
 {
-  // const point = [100,180];
+  const d = [2019,11,14];
 
-  const point = {
-    x: 100,
-    y: 180,
-  };
-
-  point.x = 120;
-  // point["y"] = 120;
-
-  // console.log(point.x);
-  // console.log(point["y"]);
-
-  point.z = 90;
-  delete point.y;
-  console.log(point);
+  console.log(d.join("/"));
+  console.log(d.join(""));
+  console.log(d.join("こ"));
 } 
